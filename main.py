9a2ad00c9de1b2ef9be1029399cdb218b69eaa4a
DieProgrammIDE <39222199+DieProgrammIDE@users.noreply.github.com>
2021-06-05 17:54:55 +0200

    Use astar-python (https://github.com/zephirdeadline/astar_python)

--- main.py
+++ main.py
@@ -2,24 +2,37 @@ import cv2
 import numpy as np
 import time
 import sys
+from astar_python.astar import Astar
+import random
 
 
 def pathfinder(point,target,obstacles):
-	areas = np.zeros((500,500), np.uint8)
+	print(point,target)
+	area = np.zeros((500,500), np.uint8)
 	frame = np.zeros((500,500,3), np.uint8)
 	for i in obstacles:
-		print(i)
-		areas[i[0]][i[1]] = 1
-		print(frame[i[0]][i[1]])
-		frame[i[0]][i[1]] = (0,0,255)
-		print(frame[i[0]][i[1]])
-	while True:
-		cv2.namedWindow("FollowMeCooler Pathfinder")
-		cv2.imshow("FollowMeCooler Pathfinder", frame)
-		key = cv2.waitKey(1) & 0xFF
-		np.set_printoptions(threshold=sys.maxsize)
-		#print(frame[frame > 0])
-		
-		time.sleep(1)
+		area[i[0]][i[1]] = 1
+		frame[i[1]][i[0]] = (255,255,255)
+		#print(i[0],i[1])
 
-pathfinder((250,10),(230,450),((240,30),(235,40)))
+	area[point[0]][point[1]]=0
+	area[target[0]][target[1]]=0
+	
+	astar = Astar(area)
+	path = astar.run(point,target)
+	for i in path:
+		frame[i[1]][i[0]] = (0,0,255)
+		#print(i)
+	cv2.namedWindow("FollowMeCooler Pathfinder")
+	cv2.imshow("FollowMeCooler Pathfinder", frame)
+	key = cv2.waitKey(1) & 0xFF
+	np.set_printoptions(threshold=sys.maxsize)
+	#print(frame[frame > 0])
+	
+	
+
+while True:
+	#time.sleep(0.1)
+	obstacles = [[random.randint(0,499),random.randint(0,499)],[random.randint(0,499),random.randint(0,499)]]
+	for i in range(0,1000): obstacles.append([random.randint(0,499),random.randint(0,499)])
+	pathfinder((100,random.randint(0,499)),(400,random.randint(0,499)),obstacles)
