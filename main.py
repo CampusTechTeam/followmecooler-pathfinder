20d4c74599ffd8eff0761888e69534e633068d97
DieProgrammIDE <39222199+DieProgrammIDE@users.noreply.github.com>
2021-06-17 20:30:29 +0200

    Optimize

--- main.py
+++ main.py
@@ -8,11 +8,51 @@ import random
 
 def pathfinder(point,target,obstacles):
 	print(point,target)
-	area = np.zeros((250,250), np.uint8)
-	frame = np.zeros((250,250,3), np.uint8)
+	area = np.zeros((253,250), np.uint8)
+	frame = np.zeros((253,253,3), np.uint8)
 	for i in obstacles:
 		area[i[0]][i[1]] = 1
 		frame[i[1]][i[0]] = (255,255,255)
+
+		area[i[0]-1][i[1]] = 5
+		frame[i[1]][i[0]-1] = (255*0.7,255*0.7,255*0.7)
+
+		area[i[0]][i[1]-1] = 5
+		frame[i[1]-1][i[0]] = (255*0.7,255*0.7,255*0.7)
+
+		area[i[0]+1][i[1]] = 5
+		frame[i[1]][i[0]+1] = (255*0.7,255*0.7,255*0.7)
+
+		area[i[0]][i[1]+1] = 5
+		frame[i[1]+1][i[0]] = (255*0.7,255*0.7,255*0.7)
+	
+		area[i[0]-2][i[1]] = 5
+		frame[i[1]][i[0]-2] = (255*0.7,255*0.7,255*0.7)
+
+		area[i[0]][i[1]-2] = 5
+		frame[i[1]-2][i[0]] = (255*0.4,255*0.4,255*0.4)
+
+		area[i[0]+2][i[1]] = 5
+		frame[i[1]][i[0]+2] = (255*0.4,255*0.4,255*0.4)
+
+		area[i[0]][i[1]+2] = 5
+		frame[i[1]+2][i[0]] = (255*0.4,255*0.4,255*0.4)
+
+		area[i[0]+1][i[1]+1] = 5
+		frame[i[1]+1][i[0]+1] = (255*0.4,255*0.4,255*0.4)
+		
+		area[i[0]-1][i[1]-1] = 5
+		frame[i[1]-1][i[0]-1] = (255*0.4,255*0.4,255*0.4)
+
+		area[i[0]-1][i[1]+1] = 5
+		frame[i[1]+1][i[0]-1] = (255*0.4,255*0.4,255*0.4)
+
+		area[i[0]+1][i[1]-1] = 5
+		frame[i[1]-1][i[0]+1] = (255*0.4,255*0.4,255*0.4)
+
+		
+
+
 		#print(i[0],i[1])
 
 	area[point[0]][point[1]]=0
@@ -21,7 +61,7 @@ def pathfinder(point,target,obstacles):
 	astar = Astar(area)
 	path = astar.run(point,target)
 	for i in path:
-		frame[i[1]][i[0]] = (0,0,255)
+		frame[i[1]][i[0]] = (3,3,255)
 		#print(i)
 	cv2.namedWindow("FollowMeCooler Pathfinder", flags=cv2.WINDOW_GUI_EXPANDED)
 	cv2.imshow("FollowMeCooler Pathfinder", frame)
@@ -32,6 +72,6 @@ def pathfinder(point,target,obstacles):
 	
 
 while True:
-	obstacles = [[random.randint(0,249),random.randint(0,249)],[random.randint(0,249),random.randint(0,249)]]
-	for i in range(0,1000): obstacles.append([random.randint(0,249),random.randint(0,249)])
-	pathfinder((100,random.randint(0,249)),(200,random.randint(0,249)),obstacles)
+	obstacles = [[random.randint(15,240),random.randint(15,240)],[random.randint(15,240),random.randint(15,240)]]
+	for i in range(3,300): obstacles.append([random.randint(15,240),random.randint(15,240)])
+	pathfinder((103,random.randint(15,240)),(203,random.randint(15,240)),obstacles)
